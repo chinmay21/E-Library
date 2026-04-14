bc7785337c214500265a21f81cbbf43af6cbac19
Chinmay <dhaundiyalchinmay@gmail.com>
2026-04-14 22:47:03 +0530

    Add onboarding and dashboard page

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,6 +6,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-up(.*)",
 ]);
 
+
 export const proxy = clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect();

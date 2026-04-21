7a0b30556a309d31534d2281bc2d659ee454569f
Chinmay <dhaundiyalchinmay@gmail.com>
2026-04-21 23:38:20 +0530

    Add admin dashboard UI

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,7 +1,11 @@
 export default function AdminDashboard() {
     return (
         <div>
-            Admin dashboard
+            <div className="bg-indigo-900 w-screen h-100">
+                <p className="text-4xl font-semibold text-blue-100 mx-auto w-fit pt-15">
+                    Welcome to Admin Dashboard
+                </p>
+            </div>
         </div>
     );
 }
